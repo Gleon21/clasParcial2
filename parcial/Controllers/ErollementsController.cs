278d63957d0c40d85864bd2101c82d4488f19c61
Gersson Leon <ll0204032017@unab.edu.sv>
2021-11-13 15:17:03 -0600

    Agregar archivos de proyecto.

---
+++ parcial/Controllers/ErollementsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using parcial.Models;
+
+namespace parcial.Controllers
+{
+    public class ErollementsController : Controller
+    {
+        private EscuelaDeConexionespaEntities db = new EscuelaDeConexionespaEntities();
+
+        // GET: Erollements
+        public ActionResult Index()
+        {
+            var erollements = db.Erollements.Include(e => e.Courses).Include(e => e.Students);
+            return View(erollements.ToList());
+        }
+
+        // GET: Erollements/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Erollements erollements = db.Erollements.Find(id);
+            if (erollements == null)
+            {
+                return HttpNotFound();
+            }
+            return View(erollements);
+        }
+
+        // GET: Erollements/Create
+        public ActionResult Create()
+        {
+            ViewBag.CourseID = new SelectList(db.Courses, "CouserId", "Title");
+            ViewBag.StudentID = new SelectList(db.Students, "StudentId", "FirstMidName");
+            return View();
+        }
+
+        // POST: Erollements/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ErollementID,CourseID,StudentID,Grade")] Erollements erollements)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Erollements.Add(erollements);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CourseID = new SelectList(db.Courses, "CouserId", "Title", erollements.CourseID);
+            ViewBag.StudentID = new SelectList(db.Students, "StudentId", "FirstMidName", erollements.StudentID);
+            return View(erollements);
+        }
+
+        // GET: Erollements/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Erollements erollements = db.Erollements.Find(id);
+            if (erollements == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CourseID = new SelectList(db.Courses, "CouserId", "Title", erollements.CourseID);
+            ViewBag.StudentID = new SelectList(db.Students, "StudentId", "FirstMidName", erollements.StudentID);
+            return View(erollements);
+        }
+
+        // POST: Erollements/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ErollementID,CourseID,StudentID,Grade")] Erollements erollements)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(erollements).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CourseID = new SelectList(db.Courses, "CouserId", "Title", erollements.CourseID);
+            ViewBag.StudentID = new SelectList(db.Students, "StudentId", "FirstMidName", erollements.StudentID);
+            return View(erollements);
+        }
+
+        // GET: Erollements/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Erollements erollements = db.Erollements.Find(id);
+            if (erollements == null)
+            {
+                return HttpNotFound();
+            }
+            return View(erollements);
+        }
+
+        // POST: Erollements/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Erollements erollements = db.Erollements.Find(id);
+            db.Erollements.Remove(erollements);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ parcial/Controllers/StudentsController.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using parcial.Models;
+
+namespace parcial.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private EscuelaDeConexionespaEntities db = new EscuelaDeConexionespaEntities();
+        
+        // GET: Students
+        public ActionResult Index()
+        {
+            return View(db.Students.ToList());
+        }
+        public ActionResult Cursos()
+        {
+            var informationofStudents = db.Students.ToList();
+            var informationofCourse = db.Courses.ToList();
+
+
+            List<SelectListItem> ComboboxOfStudents = new List<SelectListItem>();
+            List<SelectListItem> ComboboxOfCourses = new List<SelectListItem>();
+
+            foreach (var iteracion in informationofStudents)
+            {
+
+
+                ComboboxOfStudents.Add(new SelectListItem
+
+
+                {
+                    Text = iteracion.FirstMidName,
+                    Value = Convert.ToString(iteracion.StudentId)
+
+
+                }
+
+
+
+
+                    );
+
+
+                ViewBag.listofstudentcombobox = ComboboxOfStudents;
+
+
+            }
+
+
+
+
+            foreach (var iteracion in informationofCourse)
+            {
+
+
+                ComboboxOfCourses.Add(new SelectListItem
+
+
+                {
+                    Text = iteracion.Title,
+                    Value = Convert.ToString(iteracion.CouserId)
+
+
+                }
+
+
+
+
+                    );
+
+
+                ViewBag.listofcoursescombobox = ComboboxOfCourses;
+
+
+            }
+
+
+            return View();
+        }
+
+        // GET: Students/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Students students = db.Students.Find(id);
+            if (students == null)
+            {
+                return HttpNotFound();
+            }
+            return View(students);
+        }
+
+        // GET: Students/Create
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Students/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "StudentId,LastName,FirstMidName,EnrrollmentsDate")] Students students)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(students);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+           
+            return View(students);
+        }
+
+        // GET: Students/Edit/5
+        bool edit = false;
+        public ActionResult Update(int? id)
+        {
+            if (id == null)
+            {
+                return View();
+            }
+            Students students = db.Students.Find(id);
+            if (students == null)
+            {
+                return HttpNotFound();
+            }
+            edit = true;
+            return View(students);
+        }
+
+        // POST: Students/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update([Bind(Include = "StudentId,LastName,FirstMidName,EnrrollmentsDate")] Students students)
+        {
+            if (edit)
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(students).State = EntityState.Modified;
+                    db.SaveChanges();
+                    edit = false;
+                    return RedirectToAction("Index");
+                }
+            }
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(students);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+
+
+            return View(students);
+        }
+
+        // GET: Students/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Students students = db.Students.Find(id);
+            if (students == null)
+            {
+                return HttpNotFound();
+            }
+            return View(students);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Students students = db.Students.Find(id);
+            db.Students.Remove(students);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
